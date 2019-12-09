cea11ef58789183e56aacff24929121eaf3c855c
Robert Bond <bondr@nsuok.edu>
2019-12-09 10:37:18 -0600

    timezone

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/bash
 
 # Default to UTC if no TIMEZONE env variable is set
-echo "Setting time zone to ${TIMEZONE=America/Chicago}"
+echo "Setting time zone to ${TIMEZONE}"
 # This only works on Debian-based images
 echo "${TIMEZONE}" > /etc/timezone
 dpkg-reconfigure tzdata
