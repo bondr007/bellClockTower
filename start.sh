5b2955ffc3c22a3a7c7dbe34e5b1920445b5aa09
Robert Bond <bondr@nsuok.edu>
2019-12-09 11:19:19 -0600

    tz

--- start.sh
+++ start.sh
@@ -5,9 +5,12 @@
 echo "Setting time zone to ${TIMEZONE}"
 # This only works on Debian-based images
 echo "${TIMEZONE}" > /etc/timezone
+echo "${TIMEZONE}" > /etc/timezone
 echo "cat ing timezone file"
 cat /etc/timezone
 dpkg-reconfigure tzdata
+echo "current time is: "
+date
 
 # start conrtab 
 cron -f
